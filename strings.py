aa9c62f2b897a6dd536e552c78fd3e01bab53a80
Luar <143941627+RaulSoar1@users.noreply.github.com>
2026-02-27 10:17:23 -0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,6 @@
+estatura = float(input("digite sua altura"))
+estatura = estatura * 100
+
+#em python, f-strings (f"...") são usadas inserir textos traduzidos dinamicamente dentro de uma frase.
+
+print(f"sua altura é de{estatura}")
