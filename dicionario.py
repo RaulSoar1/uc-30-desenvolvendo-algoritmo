b9fa3121963735ec84e794a091bd4df95afc3a73
Luar <143941627+RaulSoar1@users.noreply.github.com>
2026-03-11 08:24:30 -0300

    Add files via upload

--- dicionario.py
+++ dicionario.py
@@ -1,97 +1,40 @@
-#sem dicionario
-matricula1 = 2026001
-
-
-
-
-
-
-
-
-
-
-
-
 contato = {
     "@camila": "camila",
     "@paola": "paola",
     "@sheron": "sheron",
-    "@bruna": "bruna"
+    "@bruna": "bruna",
+    "@joao": "joão"
 }
 
-print(contato)
-print(type(contato))
+#obter chaves
+print("chaves: ")
+for insta in contato.keys():
+    print(insta)
 
-#Acesso direto ao dicionario
-print(contato["@camila"])
+#obter valores
+print("\n valores:")
+for nome in contato.values():
+    print(nome)
 
-#acesso seguro com get()
-print(contato.get("@paola"))
-print(contato.get("@inexistente"))
-print(contato.get("@inexistente", "não encontrado"))
+#obter pares (chave-valor)
+print("\n pares: ") 
+for insta, nome in contato.items():
+    print(f"{insta} --> {nome}")
 
-print("original:", contato) #acessando a lista original
+    contato = {
+    "@camila": 1.66,
+    "@paola": 1.50,
+    "@sheron": 1.80,
+    "@bruna": 1.60,
+    "@joao": 1.70
+}
+# ordenar por chave
+print("ordenar por chave: ")
+for insta in sorted(contato.keys()):
+    print(f"{insta} --> {contato[insta]:.2f}m")
 
-#Add novo elemento
-contato["@giovanna"] = "Giovanna"
-print("Após add: ", contato)
-
-#atualiza elemento existente
-contato["@paola"] = "paola oliveira"
-print("Após add: ", contato)
-
-contato.update({
-        "@bruna": "Bruna Marquezine",
-        "@camila": "camila Q"
-})
-
-print("Após atualização: ", contato)
-
-#pop: remove e retorna
-removido = contato.pop("@sheron")
-print(f"removido: {removido}")
-print("Após o del: ", contato)
-
-# del remove sem retornar
-del contato["@paola"]
-print("Após o del: ", contato)
-
-
-#clear esvazia tudo
-copia = dict(contato)
-contato.clear()
-print("Após clear: ", contato)
-print("cópia: ", copia)
-
-print("número de contato: ", len(contato))
-
-contato.pop("@camila")
-print("Após remover um: ", len(contato))
-
-#verificar existência
-if "@joao" in contato:
-    print(f"encontrado: {contato['@joao']}")
-
-    if "@inexistente" in contato:
-        print("existe")
-else:
-    print("não existe.")
-
-
-
-
-#dicionário vazio
-vazio = {}
-
-#dicionario com tipos mistos
-dados = {
-    "nome": "joão",
-    "idade": 25,
-    "altura": 1.70,
-    "ativo": True
-    }
-
-print
-
-("vazio: ", vazio)
-print("vazio: ", dados)
+#Ordenar por valor
+from operator  import itemgetter
+print("\n Ordenando por valor (altura): ")
+for insta, estatura in sorted(contato.items(), key=itemgetter(1)):
+        print(f"{insta} --> {estatura:.2f}m")
